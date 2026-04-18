cc9ed046816f37fdb9b9e793f03f42e9fc57cae9
Anurag Sen <anurag007sen@gmail.com>
2026-04-18 07:09:47 +0530

    root navroute added

---
+++ app/src/main/java/com/content/boockreaderapp/navigation/RootNavigationRoute.kt
@@ -0,0 +1,16 @@
+package com.content.boockreaderapp.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object DashboardScreenRoute
+
+@Serializable
+object BookDetailsRoute
+
+@Serializable
+object BookReadingRoute
+
+@Serializable
+object SplashRoute
+
